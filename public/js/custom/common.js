c31b544768e47091e175ffffa9d18366c6248fcc
tuetranduy <tue1996@gmail.com>
2018-12-04 19:20:20 +0700

    add product description, remove scroll bar

---
+++ public/js/custom/common.js
@@ -0,0 +1,3 @@
+$("body").niceScroll({
+  cursorwidth:6
+});
---
+++ public/js/custom/index.js
@@ -0,0 +1,9 @@
+function showDescription(product) {
+  var description = document.getElementById(product)
+  description.style.visibility = "visible"
+}
+
+function hideDescription(product) {
+  var description = document.getElementById(product)
+  description.style.visibility = "hidden"
+}
