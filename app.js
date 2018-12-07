73f713fe5099ac57759c3450cfb75bb000461f57
tuetranduy <tue1996@gmail.com>
2018-12-07 22:30:15 +0700

    add caching, product render via db

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
+var cache = require('memory-cache');
 
 var indexRouter = require("./routes/index");
+var _ = require('lodash');
 
 var app = express();
+app.locals._ = _;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -15,20 +18,43 @@ app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 
-app.use("/", indexRouter);
+// caching
+let memCache = new cache.Cache();
+let cacheMiddleware = (duration) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cacheContent = memCache.get(key);
+    if (cacheContent) {
+      res.send(cacheContent);
+      return
+    } else {
+      res.sendResponse = res.send
+      res.send = (body) => {
+        memCache.put(key, body, duration * 1000);
+        res.sendResponse(body)
+        console.log("Cache is: " + body)
+      }
+      next()
+    }
+  }
+}
+
+app.use("/", cacheMiddleware(30), indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -36,7 +62,7 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render("error");
-  console.log("============ "+ err + " ===============")
+  console.log("============ " + err + " ===============")
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,31 @@ var express = require("express");
 var router = express.Router();
 var axios = require("axios");
 var config = require("../config/config")
+var _ = require('lodash');
+
+function getProducts() {
+  return axios.get(config.application.API_URL + `getAllProduct`)
+    .then(response => {
+      console.log(response.data.data)
+      return response.data.data
+    }).catch(function (error) {
+      console.log("Error: " + error)
+    })
+}
 
 /* GET home page. */
 router.get("/", function (req, res) {
 
   // res.clearCookie("token")
 
-  res.render("screens/index", {
-    title: "Home Page"
-  });
+  getProducts(req).then((data) => {
+
+    res.render("screens/index", {
+      title: "Home Page",
+      products: data
+    });
+
+  })
 
 });
 
